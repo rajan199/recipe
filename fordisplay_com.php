b67550edc6b86c5559b9feb256c371ce3409a612
Rajan <ranarajan49@gmail.com>
2017-02-06 20:58:41 +0530

    php

---
+++ fordisplay_com.php
@@ -0,0 +1,206 @@
+
+<!DOCTYPE html>
+<html>
+<head>
+<title>Big shope A Ecommerce Category Flat Bootstarp Resposive Website Template | Home :: w3layouts</title>
+<link href="css/bootstrap.css" rel="stylesheet" type="text/css" media="all" />
+<!--theme-style-->
+<link href="css/style.css" rel="stylesheet" type="text/css" media="all" />	
+<!--//theme-style-->
+<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+<script type="application/x-javascript"> addEventListener("load", function() { setTimeout(hideURLbar, 0); }, false); function hideURLbar(){ window.scrollTo(0,1); } </script>
+<!--fonts-->
+<link href='http://fonts.googleapis.com/css?family=Open+Sans:400,300,600,700,800' rel='stylesheet' type='text/css'>
+<!--//fonts-->
+<script src="js/jquery.min.js"></script>
+<!--script-->
+</head>
+<body> 
+
+<?php
+
+include 'header.php';
+
+?>
+
+
+
+<div class="container">
+
+<div class="row">
+<div class="col-md-12">
+<?php
+include 'alphaser.php';
+
+?>
+</div>
+</div>
+</div>
+
+<!---->
+	<div class="container">
+			<div class="shoes-grid">
+			<a href="single.html">
+			<div class="wrap-in">
+				<div class="wmuSlider example1 slide-grid">		 
+				   <div class="wmuSliderWrapper">		  
+					   <article style="position: absolute; width: 100%; opacity: 0;">					
+						<div class="banner-matter">
+						<div class="col-md-5 banner-bag">
+							<img class="img-responsive " src="images/bag.jpg" alt=" " />
+							</div>
+							<div class="col-md-7 banner-off">							
+								<h2>FLAT 50% 0FF</h2>
+								<label>FOR ALL PURCHASE <b>VALUE</b></label>
+								<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et </p>					
+								<span class="on-get">GET NOW</span>
+							</div>
+							
+							<div class="clearfix"> </div>
+						</div>
+						
+					 	</article>
+					 	<article style="position: absolute; width: 100%; opacity: 0;">					
+						<div class="banner-matter">
+						<div class="col-md-5 banner-bag">
+							<img class="img-responsive " src="images/bag1.jpg" alt=" " />
+							</div>
+							<div class="col-md-7 banner-off">							
+								<h2>FLAT 50% 0FF</h2>
+								<label>FOR ALL PURCHASE <b>VALUE</b></label>
+								<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et </p>					
+								<span class="on-get">GET NOW</span>
+							</div>
+							
+							<div class="clearfix"> </div>
+						</div>
+						
+					 	</article>
+					 	<article style="position: absolute; width: 100%; opacity: 0;">					
+						<div class="banner-matter">
+						<div class="col-md-5 banner-bag">
+							<img class="img-responsive " src="images/bag.jpg" alt=" " />
+							</div>
+							<div class="col-md-7 banner-off">							
+								<h2>FLAT 50% 0FF</h2>
+								<label>FOR ALL PURCHASE <b>VALUE</b></label>
+								<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et </p>					
+								<span class="on-get">GET NOW</span>
+							</div>
+							
+							<div class="clearfix"> </div>
+						</div>
+						
+					 	</article>
+						
+					 </div>
+					 </a>
+	                <ul class="wmuSliderPagination">
+	                	<li><a href="#" class="">0</a></li>
+	                	<li><a href="#" class="">1</a></li>
+	                	<li><a href="#" class="">2</a></li>
+	                </ul>
+					 <script src="js/jquery.wmuSlider.js"></script> 
+				  <script>
+	       			$('.example1').wmuSlider();         
+	   		     </script> 
+	            </div>
+	          </div>
+	           	</a>
+
+<!--
+<div class="products">
+	   		     	<h5 class="latest-product">LATEST PRODUCTS</h5>	
+	   		     	  <a class="view-all" href="product.html">VIEW ALL<span> </span></a> 		     
+	   		     </div>
+<div class="panel panel-default">
+  <div class="panel-body">
+    <div class="row">
+					
+	
+	 ?php 
+ // include 'database.php';
+//$res=new database();
+//$con=$res->connection();  
+
+
+//$cnt=$res->companybyid($_REQUEST["id"],$con);
+$id=$_REQUEST["id"];
+$con=mysql_connect('localhost','root','');
+    mysql_select_db('medicine',$con);
+    
+	$res=mysql_query("select * from product_tbl where company_id='$id'");
+
+//$cnt=mysql_query("select p.*,c.cat_name from pro_tbl as p,cat as c where p.fk_cat_id=c.cat_id",$con);
+
+
+  while($row=mysql_fetch_assoc($res))
+  {
+	
+ echo' <div class="col-sm-6 col-md-4">';
+   echo  '<div class="thumbnail">';
+     echo ' <img src="http://localhost:3000'.$row["product_img"].'"  style="height: 145px;"></img>';
+ echo    '<div class="caption">
+        <h3>'.$row["product_name"].'</h3>
+  <h3>'.$row["product_price"].'</h3>
+               
+        <p><button type="button" style="width: 200px;" name="btnbuy" class="btn btn-default btn-lg">
+  <span class="glyphicon glyphicon-buy" aria-hidden="true"></span> Buy
+</button>
+<button type="button" style="width: 200px; background:#323A45; color:white; " name="btncar" class="btn btn-default btn-lg">
+  <span class="glyphicon glyphicon-shopping-cart" aria-hidden="true"></span> Add to cart
+</button>
+</p>
+
+	<div class="star-price">
+	   		     				<div class="dolor-grid"> 
+		   		     				  <span class="rating">
+									        <input type="radio" class="rating-input" id="rating-input-1-5" name="rating-input-1">
+									        <label for="rating-input-1-5" class="rating-star1"> </label>
+									        <input type="radio" class="rating-input" id="rating-input-1-4" name="rating-input-1">
+									        <label for="rating-input-1-4" class="rating-star1"> </label>
+									        <input type="radio" class="rating-input" id="rating-input-1-3" name="rating-input-1">
+									        <label for="rating-input-1-3" class="rating-star"> </label>
+									        <input type="radio" class="rating-input" id="rating-input-1-2" name="rating-input-1">
+									        <label for="rating-input-1-2" class="rating-star"> </label>
+									        <input type="radio" class="rating-input" id="rating-input-1-1" name="rating-input-1">
+									        <label for="rating-input-1-1" class="rating-star"> </label>
+							    	   </span>
+	   		     				</div>
+	   		     		</div>
+      </div>
+    </div>
+  </div>
+  ';  
+	  
+  }
+ 
+	?>
+	
+			
+</div>
+</div>
+</div>
+	
+	-->
+	
+	<?php
+	
+	
+	include 'exequery.php';
+	
+	
+	?>
+	
+<?php
+
+include 'sidebar.php';
+
+?>	   		    
+
+<?php
+
+include 'footer.php';
+?>	
+	</body>
+	</html>
